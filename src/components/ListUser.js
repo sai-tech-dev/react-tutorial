a6f61fc390f63a875ea548e3ee8d0b968f964ade
Sai Vignesh <sai3125.be@gmail.com>
2023-09-14 20:53:47 +0530

    get complete

--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function ListUser(){
 
+    const [users, setUsers] = useState([]);
     useEffect(() => {
         getUsers();
     },[]);
@@ -10,6 +12,40 @@ export default function ListUser(){
     function getUsers(){
         axios.get('http://localhost:80/api/user/save').then(function(response){
             console.log(response.data);
+            setUsers(response.data);
         })
     }
+    return(
+        <div>
+            <h1>List Users</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Mobile</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user,key)=>
+                        <tr key={key}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.mobile}</td>
+                            <td>
+                                <Link to={`user/${user.id}/edit`}>Edit</Link>
+                                <button>Delete</button>
+                            </td>
+
+
+
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
 }
