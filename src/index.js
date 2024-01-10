ad68866d7db33875eccd77847b256c630ac2faa8
Sai Vignesh <sai3125.be@gmail.com>
2024-01-10 21:23:42 +0530

    Small changes

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <>
-    <Garage />
-    {/* <Header /> */}
+    {/* <Garage /> */}
+    <Header />
     {/* <App /> */}
     {/* function_state */}
     {/* <FavoriteColor /> */}
